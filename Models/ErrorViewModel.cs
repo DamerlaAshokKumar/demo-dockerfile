903b70ba8f2db6d5730d390f6483e5e481e45546
DamerlaAshokkumar <damerlaashokkumar@gmail.com>
2022-01-31 22:09:16 +0530

    craeted anew mvc

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _45544s.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
